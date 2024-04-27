825dd6dea06dfddf0989a04a5f5c94c6bdceb3e5
Victor Leonardo José da Silva <victorl.jose.silva@gmail.com>
2024-04-26 23:52:02 -0300

    criando a função para monitorar recursos de hardware

--- monitorar_recursos.py
+++ monitorar_recursos.py
@@ -1,3 +1,22 @@
 import psutil
 import logging
 
+def monitorar_recursos():
+    try:
+        uso_cpu = psutil.cpu_percent()
+        uso_memoria = psutil.virtual_memory().percent
+        uso_disco = psutil.disk_usage('/').percent
+
+        print(f"Uso da CPU: {uso_cpu}%")
+        print(f"Uso de Memória: {uso_memoria}%")
+        print(f"Uso de Disco: {uso_disco}%")
+
+        logging.basicConfig(filename='monitoramento_recursos.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+        logging.info(f"Uso da CPU: {uso_cpu}%")
+        logging.info(f"Uso de Memória: {uso_memoria}%")
+        logging.info(f"Uso de Disco: {uso_disco}%")
+    except Exception as e:
+        print(f"Erro ao monitorar recursos do sistema: {e}")
+        logging.error(f"Erro ao monitorar recursos do sistema: {e}")
+
+
