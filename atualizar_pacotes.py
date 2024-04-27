bb14b9f3f59ecdc12b3c9ae6f870464d56a5ccb6
Victor Leonardo José da Silva <victorl.jose.silva@gmail.com>
2024-04-26 23:30:27 -0300

    criando a função para verificar permissão de superusuario

--- atualizar_pacotes.py
+++ atualizar_pacotes.py
@@ -3,3 +3,8 @@ import subprocess
 import logging
 import socket
 
+def verificar_permissao_superusuario():
+    if os.geteuid() != 0:
+        print("Erro: Esse script precisa ser executado com permissões de superusuário (root).")
+        exit(1)
+
